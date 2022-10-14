90c9f15c7228db472ec95bfdba000b21c684184a
Shruti Yendhe <72744656+shrutiyendhe@users.noreply.github.com>
2022-10-14 17:47:48 +0530

    Create Q5-pattern.java

---
+++ Day-13_10_2022/Q5-pattern.java
@@ -0,0 +1,23 @@
+/*
+  Q5) Write a Java program to display the following pattern.
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+ 
+*/
+
+public class pattern 
+{
+ 
+    public static void main(String[] args) 
+    {
+        // printing java name pattern
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+      
+    }
+}
